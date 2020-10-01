c81c6d55c980aa5842b7af11553609651fe34828
e <2292474203@qq.com>
2020-10-01 21:15:47 +0800

    Site updated: 2020-10-01 21:15:47

--- dist/music.js
+++ dist/music.js
@@ -8,7 +8,7 @@ const ap = new APlayer({
     theme: '#e9e9e9', //切换音频时的主题色，优先级低于audio.theme
     preload: 'none', //音频预加载，可选值: 'none', 'metadata', 'auto'
     mutex: true, //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
-    lrcType: 3, //歌词格式，可选值：3（LRC文件歌词格式），1（JS字符串歌词格式）
+    lrcType: 1, //歌词格式，可选值：3（LRC文件歌词格式），1（JS字符串歌词格式）
     volume: 0.7, //默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效
     fixed: true, //吸底模式（fixed:true），迷你模式（mini:true），普通模式（注释此行或者设置fixed:false）
     audio: [{ 
